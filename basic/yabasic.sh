0e53f07782062d6724270eeb2fc705d761945e00
WooYoung Moon <wymoon2690@gmail.com>
2021-04-14 14:49:05 +0900

    일부 내용 수정

---
+++ basic/yabasic.sh
@@ -0,0 +1,2 @@
+yabasic -bind ./basic/yabasic.exe ./basic/yabasic.yab
+./basic/yabasic.exe
--- run.sh
+++ run.sh
@@ -1,16 +1,17 @@
-dotnet run --project ./csharp/csharp.csproj
-go run ./go/go.go
-julia ./julia/julia.jl
-Rscript ./R/r.r
-python3 ./python/python.py
-node ./nodejs/nodejs.js
-gcc ./C/gcc.c -o ./C/gcc.out | ./C/gcc.out
-java ./java/main.java
-gfortran-9 -o ./fortran/fortran.out ./fortran/main.f90 | ./fortran/fortran.out
-ruby ./ruby/main.ru
+#dotnet run --project ./csharp/csharp.csproj
+#go run ./go/go.go
+#julia ./julia/julia.jl
+#Rscript ./R/r.r
 
-nasm -f elf64 ./assembly/main.asm -o ./assembly/assembly.obj
-gcc -no-pie ./assembly/assembly.obj -o ./assembly/assembly.bin
-./assembly/assembly.bin
+#node ./nodejs/nodejs.js
+#gcc ./C/gcc.c -o ./C/gcc.out | ./C/gcc.out
+#java ./java/main.java
+#gfortran-9 -o ./fortran/fortran.out ./fortran/main.f90 | ./fortran/fortran.out
+#ruby ./ruby/main.ru
 
-cobc -x -free ./cobol/*.cobc -o ./main.bin && ./main.bin
+#cobc -x -free ./cobol/*.cobc -o ./main.bin && ./main.bin
+
+./cobol/run.sh
+#./basic/yabasic.sh
+#./python/run.sh
+#./assembly/run.sh
